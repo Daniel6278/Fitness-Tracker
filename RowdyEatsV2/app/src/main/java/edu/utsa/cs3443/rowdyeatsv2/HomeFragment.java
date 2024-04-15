b24e2d4f0d10b93fcc0cee0cc141e5daaf00cc99
williamqin123 <willqin123@gmail.com>
2024-04-15 13:37:51 -0500

    navbar

---
+++ RowdyEatsV2/app/src/main/java/edu/utsa/cs3443/rowdyeatsv2/HomeFragment.java
@@ -0,0 +1,22 @@
+package edu.utsa.cs3443.rowdyeatsv2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.io.*;
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment(){
+        // require a empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
