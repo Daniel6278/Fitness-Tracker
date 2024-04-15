12d8fb11ebbb19a2f6e35347a11abfa23db816ef
Daniel6278 <danielvigil46@gmail.com>
2024-04-15 13:47:20 -0500

    Test

--- RowdyEatsV2/app/src/main/java/edu/utsa/cs3443/rowdyeatsv2/HomeFragment.java
+++ RowdyEatsV2/app/src/main/java/edu/utsa/cs3443/rowdyeatsv2/HomeFragment.java
@@ -4,6 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.*;
 import androidx.fragment.app.Fragment;
@@ -14,9 +21,25 @@ public class HomeFragment extends Fragment {
         // require a empty public constructor
     }
 
+    private boolean isMenu1Visible = false;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        Button btnMenu1 = getView().findViewById(R.id.btnMenu1);
+        final TextView txtDropdown1 = getView().findViewById(R.id.txtDropdown1);
+
+        btnMenu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Toggle visibility of the dropdown text
+                isMenu1Visible = !isMenu1Visible;
+                txtDropdown1.setVisibility(isMenu1Visible ? View.VISIBLE : View.GONE);
+            }
+        });
+
+        // Repeat for other buttons and text views
     }
 }
--- RowdyEatsV2/app/src/main/java/edu/utsa/cs3443/rowdyeatsv2/MainActivity.java
+++ RowdyEatsV2/app/src/main/java/edu/utsa/cs3443/rowdyeatsv2/MainActivity.java
@@ -8,24 +8,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean isMenu1Visible = false;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnMenu1 = findViewById(R.id.btnMenu1);
-        final TextView txtDropdown1 = findViewById(R.id.txtDropdown1);
-
-        btnMenu1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Toggle visibility of the dropdown text
-                isMenu1Visible = !isMenu1Visible;
-                txtDropdown1.setVisibility(isMenu1Visible ? View.VISIBLE : View.GONE);
-            }
-        });
 
         // Repeat for other buttons and text views
     }
