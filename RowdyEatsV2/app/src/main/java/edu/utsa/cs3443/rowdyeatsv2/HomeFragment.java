0f699a6fa9dd5717544253aac709722bafaf10e8
williamqin123 <willqin123@gmail.com>
2024-04-15 14:18:20 -0500

    views

--- RowdyEatsV2/app/src/main/java/edu/utsa/cs3443/rowdyeatsv2/HomeFragment.java
+++ RowdyEatsV2/app/src/main/java/edu/utsa/cs3443/rowdyeatsv2/HomeFragment.java
@@ -10,6 +10,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.*;
@@ -24,16 +27,22 @@ public class HomeFragment extends Fragment {
     private boolean isMenu1Visible = false;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
 
-        Button btnMenu1 = getView().findViewById(R.id.btnMenu1);
-        final TextView txtDropdown1 = getView().findViewById(R.id.txtDropdown1);
+        Button btnMenu1 = view.findViewById(R.id.btnMenu1);
+        final TextView txtDropdown1 = view.findViewById(R.id.txtDropdown1);
 
         btnMenu1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View _view) {
                 // Toggle visibility of the dropdown text
                 isMenu1Visible = !isMenu1Visible;
                 txtDropdown1.setVisibility(isMenu1Visible ? View.VISIBLE : View.GONE);
