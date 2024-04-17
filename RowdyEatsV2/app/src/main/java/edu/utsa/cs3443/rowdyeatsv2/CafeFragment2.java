0177e1c302b05cb8eb1ead09cac6be4a656cf5f8
williamqin123 <willqin123@gmail.com>
2024-04-17 14:13:45 -0500

    Update CafeFragment2.java

--- RowdyEatsV2/app/src/main/java/edu/utsa/cs3443/rowdyeatsv2/CafeFragment2.java
+++ RowdyEatsV2/app/src/main/java/edu/utsa/cs3443/rowdyeatsv2/CafeFragment2.java
@@ -20,7 +20,7 @@ public class CafeFragment2 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_recipes, container, false);
+        return inflater.inflate(R.layout.fragment_cafe, container, false);
     }
 
     @Override
