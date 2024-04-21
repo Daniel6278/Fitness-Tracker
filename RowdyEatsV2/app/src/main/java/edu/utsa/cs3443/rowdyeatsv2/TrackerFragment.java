f2c3199047900ad6c265ddd125564212d7057540
williamqin123 <willqin123@gmail.com>
2024-04-21 00:09:20 -0500

    removed unused inport

--- RowdyEatsV2/app/src/main/java/edu/utsa/cs3443/rowdyeatsv2/TrackerFragment.java
+++ RowdyEatsV2/app/src/main/java/edu/utsa/cs3443/rowdyeatsv2/TrackerFragment.java
@@ -1,7 +1,6 @@
 package edu.utsa.cs3443.rowdyeatsv2;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
