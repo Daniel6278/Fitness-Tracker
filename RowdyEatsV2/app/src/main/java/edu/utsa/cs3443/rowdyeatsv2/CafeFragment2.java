b5a835a57f0b950d98beee6d995d08d713eb1dc3
Daniel6278 <danielvigil46@gmail.com>
2024-05-01 12:43:46 -0500

    Model Update

---
+++ RowdyEatsV2/app/src/main/java/edu/utsa/cs3443/rowdyeatsv2/CafeFragment2.java
@@ -0,0 +1,23 @@
+package edu.utsa.cs3443.rowdyeatsv2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class CafeFragment2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_cafe, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
---
+++ RowdyEatsV2/app/src/main/java/edu/utsa/cs3443/rowdyeatsv2/data/CustomDataAdapter.java
@@ -0,0 +1,76 @@
+package edu.utsa.cs3443.rowdyeatsv2.data;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import edu.utsa.cs3443.rowdyeatsv2.Model.FoodRecord;
+import edu.utsa.cs3443.rowdyeatsv2.R;
+
+import java.util.ArrayList;
+
+public class CustomDataAdapter extends ArrayAdapter<FoodRecord> {
+    private int layoutResource;
+    private Activity activity;
+    private ArrayList<FoodRecord> foodArrayList;
+
+    public CustomDataAdapter(@NonNull Activity act, int resource, ArrayList<FoodRecord> data) {
+        super(act, resource, data);
+        this.layoutResource = resource;
+        this.activity = act;
+        this.foodArrayList = data;
+    }
+
+    @Override
+    public int getCount() {
+        return foodArrayList.size();
+    }
+
+    @Nullable
+    @Override
+    public FoodRecord getItem(int position) {
+        return foodArrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View row = convertView;
+        ViewHolder holder;
+
+        if (row == null) {
+            LayoutInflater inflater = LayoutInflater.from(activity);
+            row = inflater.inflate(layoutResource, parent, false);
+            holder = new ViewHolder();
+            holder.foodName = row.findViewById(R.id.name);
+            holder.foodCalories = row.findViewById(R.id.calories);
+            holder.foodDate = row.findViewById(R.id.dateText);
+            row.setTag(holder);
+        } else {
+            holder = (ViewHolder) row.getTag();
+        }
+
+        FoodRecord food = getItem(position);
+        if (food != null) {
+            holder.foodName.setText(food.getFoodName());
+            holder.foodCalories.setText(String.format("%d cal", food.getCalories()));
+            holder.foodDate.setText(food.getRecordDate());
+        }
+
+        return row;
+    }
+
+    public class ViewHolder {
+        TextView foodName, foodCalories, foodDate;
+    }
+}
